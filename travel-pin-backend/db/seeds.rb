21b0af5788a05b8c29a58b90d77cb491fdeb4907
github username <github email address>
2020-02-11 11:29:50 -0800

    seed city data from csv

--- travel-pin-backend/db/seeds.rb
+++ travel-pin-backend/db/seeds.rb
@@ -6,16 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-alicia = User.create(name: "alicia")
-xtina = User.create(name: "christina")
+require 'csv'
 
-tokyo = City.create(name: "Tokyo", country:"Japan", lat: 1.55, lng: 10.3)
-nyc = City.create(name: "New York City", country:"USA", lat: 5.55, lng: 8.3)
+# alicia = User.create(name: "alicia")
+# xtina = User.create(name: "christina")
 
-a1 = Board.create(user_id: alicia.id, note: "hello!")
-c1 = Board.create(user_id: xtina.id, note: "hola!")
+# a1 = Board.create(user_id: alicia.id, note: "hello!")
+# c1 = Board.create(user_id: xtina.id, note: "hola!")
 
-p1 = Photo.create(board_id: a1.id, city_id: tokyo.id, photographer: "coffee", download:"download")
-p2 = Photo.create(board_id: c1.id, city_id: nyc.id, photographer: "beans", download:"download2")
+CSV.foreach(Rails.root.join('db/worldcities.csv'), headers: true) do |row|
+    
+    City.create({
+        name: row[1], 
+        country:row[4], 
+        lat: row[2], 
+        lng: row[3]
+    })
+
+end
 
 
