74838ca1b9acf5d0ceb30e0707d20de2f461deab
github username <github email address>
2020-02-12 13:36:49 -0800

    search changes

--- travel-pin-backend/app/controllers/cities_controller.rb
+++ travel-pin-backend/app/controllers/cities_controller.rb
@@ -1,12 +1,12 @@
 class CitiesController < ApplicationController
     def index
-        @cities = City.all 
+        cities = City.all 
         render json: cities
     end
     
     def show 
-        @city = City.find(params[:id])
-        render json: @city
+        city = City.find(params[:id])
+        render json: city
     end
 
     # def create
