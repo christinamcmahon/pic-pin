3cf23bba4742f67c344d89482bc015e6e60c096f
github username <github email address>
2020-02-12 16:02:46 -0800

    fix login and signup

--- travel-pin-frontend/src/index.js
+++ travel-pin-frontend/src/index.js
@@ -1,39 +1,90 @@
-// ES Modules syntax
-import Unsplash from 'unsplash-js';
-// require syntax
-const Unsplash = require('unsplash-js').default;
-// const unsplash = new Unsplash({ accessKey: "{APP_ACCESS_KEY}" });
-// const unsplash = new Unsplash({
-//   accessKey: "{APP_ACCESS_KEY}",
-//   // Optionally you can also configure a custom header to be sent with every request
-//   headers: {
-//     "X-Custom-Header": "foo"
-//   },
-//   // Optionally if using a node-fetch polyfill or a version of fetch which supports the timeout option, you can configure the request timeout for all requests
-//   timeout: 500 // values set in ms
-// });
+// unsplash api links
+let API_BASE_URL = "https://api.unsplash.com/search/photos?&client_id=28dcfd24d5d678c88e38fcb751323ba6bae6b9eec6d3d514f24f645deb90b2b4&query="
+let SEARCH = {};
 
+// login links
+const BASE_URL = "http://localhost:3000"
+const USER_URL = `${BASE_URL}/users`
+let currentUser = null
 
-document.addEventListener("DOMContentLoaded",() => {
-    getUnsplash();
-});
+document.addEventListener('DOMContentLoaded', event => {
+    signupForm()
+    loginForm()
+})
 
-function getUnsplash(){
-    return fetch("https://api.unsplash.com/", {
-        method: "GET",
-        headers: {
-            "Content-Type" : "application/json",
-            "Accept-Version" : "v1"
-        }
+// signup 
+function signupForm() {
+    let signup = document.getElementById("signup")
+    signup.addEventListener("submit", event => {
+        event.preventDefault()
+        let user = { name: event.target.name.value }
+        postUser(user)
     })
-        .then(res=>res.json())
-        .then(photo=>console.log(photo))
-        // .then(photo=>showPhoto(photo))
-        .catch(err=>console.log(err))
 }
 
-function showPhoto(photo){
-    console.log(photo)
-    const unsplash = new Unsplash({ accessKey: "{APP_ACCESS_KEY}" });
+function postUser(user) {
+    console.log(user);
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+}
+
+// login
+function loginForm() {
+    let login = document.getElementById("login")
+    login.addEventListener("submit", event => {
+        event.preventDefault()
+        let user = { name: event.target.name.value }
+        loginUser(user)
+    })
+}
+
+function loginUser(user) {
+    fetch(USER_URL)
+        .then(res => res.json())
+        .then(data => validUser(data, user))
+        .catch(err => console.log(err))
+}
+
+function validUser(data, user) {
+    console.log(data);
+    let currentUserH3 = document.getElementById("current-user")
+    let userExists = data.find(u => u.name == user.name)
+    if (userExists) {
+        currentUser = userExists
+        currentUserH3.textContent = currentUser.name
+        //render explore page here??
+        console.log(currentUser)
+    } else {
+        alert("User Does Not Exist")
+    }
+}
+
+
+// fetch pics from unsplash api
+function fetchPics() {
+    event.preventDefault();
+    const search_input = document.querySelector('input[type="search"]');
+    
+    console.log(search_input.value);
+  const search = search_input.value;
+  fetch( `${API_BASE_URL}${search}`)
+      
+    .then((res) => {
+          return res.json();
+      })
+      .then((jsonData) => {
+          console.log(jsonData);
+          
+      }).catch((error) => {
+          console.error("Fetch pictures Error", error);
+      });
 
 }
